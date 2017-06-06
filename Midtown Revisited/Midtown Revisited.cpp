72cfe0d43f48fc2c4a5b657219091e1fb69d2fdb
Sam Wilde <samuelwilde@hotmail.com>
2017-06-06 21:50:57 +0100

    Added lots of the most important hooks

--- Midtown Revisited/Midtown Revisited.cpp
+++ Midtown Revisited/Midtown Revisited.cpp
@@ -1,3 +1,531 @@
 
 #include "stdafx.h"
 #include "Midtown Revisited.h"
+
+#include <ctime>
+#include <valarray>
+#include <cstdio>
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+#define DIRECTX_VERSION 0x0700
+
+#ifdef DIRECTX_VERSION
+# ifndef DIRECT3D_VERSION
+#  define DIRECT3D_VERSION DIRECTX_VERSION
+# endif
+# ifndef DIRECTDRAW_VERSION
+#  define DIRECTDRAW_VERSION DIRECTX_VERSION
+# endif
+# ifndef DIRECTINPUT_VERSION
+#  define DIRECTINPUT_VERSION DIRECTX_VERSION
+# endif
+# ifndef DIRECTSOUND_VERSION
+#  define DIRECTSOUND_VERSION DIRECTX_VERSION
+# endif
+#endif
+
+#include <d3d.h>
+#include <ddraw.h>
+#include <dinput.h>
+
+#include "memHandle.h"
+
+#define ASSERT_SIZE(type, size) static_assert(sizeof(type) == size, "Invalid "#type" size");
+
+template <typename... T>
+inline void DebugPrint(const char* format, T&&... args)
+{
+    char buffer[2048];
+    std::snprintf(buffer, std::size(buffer), format, std::forward<T>(args)...);
+
+    OutputDebugStringA(std::string("[Midtown Revisited] | ").append(buffer).append("\n").c_str());
+}
+
+enum class HookType
+{
+    JMP,
+    CALL,
+    PUSH,
+
+    COUNT,
+};
+
+const char* HookTypeNames[static_cast<std::size_t>(HookType::COUNT)] =
+{
+    "JMP",
+    "CALL",
+    "PUSH"
+};
+
+void CreateHook(const char* name, const char* description, memHandle pHook, memHandle pDetour, HookType type)
+{
+    std::intptr_t RVA = pDetour.as<std::intptr_t>() - pHook.offset(5).as<std::intptr_t>();
+
+    switch (type)
+    {
+        case HookType::JMP:
+        {
+            pHook.write_args_vp<std::uint8_t, std::intptr_t>(0xE9, RVA);
+        } break;
+
+        case HookType::CALL:
+        {
+            pHook.write_args_vp<std::uint8_t, std::intptr_t>(0xE8, RVA);
+        } break;
+
+        case HookType::PUSH:
+        {
+            pHook.write_args_vp<std::uint8_t, std::uintptr_t>(0x68, pDetour.as<std::uintptr_t>());
+        } break;
+    }
+
+    DebugPrint(
+        "Created %s %s hook at %X pointing to %X - %s",
+        name,
+        HookTypeNames[static_cast<std::size_t>(type)],
+        pHook.as<std::uintptr_t>(),
+        pDetour.as<std::uintptr_t>(),
+        description
+    );
+}
+
+struct gfxInterface
+{
+    enum gfxDeviceType
+    {
+        Software        = 0, // Software (No 3D Video Card)
+        Hardware        = 1, // Hardware (3D Video Card)
+        HardwareWithTnL = 2  // Hardware (3D Video Card With T&L)
+    };
+
+    enum gfxDepthFlags
+    {
+        Depth16 = 0x400,
+        Depth24 = 0x200,
+        Depth32 = 0x100
+    };
+
+    struct gfxResData
+    {
+        unsigned short ScreenWidth;
+        unsigned short ScreenHeight;
+        unsigned short ColorDepth;
+        unsigned short Flags; // = 0b110 | (ColorDepth == 16)
+    };
+
+    GUID GUID;
+    char Name[64];
+
+    unsigned int DeviceCaps;
+
+    gfxDeviceType DeviceType;
+
+    unsigned int ResolutionCount;   // Max of 64 resolutions
+    unsigned int ResolutionChoice;
+
+    gfxDepthFlags AcceptableDepths;  // Used to check if mmResolution::Depth is allowed
+
+    unsigned int AvailableMemory;
+    unsigned int VendorID;
+    unsigned int DeviceID;
+
+    gfxResData Resolutions[64];
+};
+
+ASSERT_SIZE(gfxInterface, 624);
+
+auto& sm_EnableSetLOD = memHandle(0x684D34).as<bool&>();
+auto& sm_Allow32 = memHandle(0x684D36).as<bool&>();
+auto& hWndParent = memHandle(0x682FA0).as<HWND&>();
+auto& hWndMain = memHandle(0x6830B8).as<HWND&>();
+auto& lpWindowTitle = memHandle(0x68311C).as<LPCSTR&>();
+auto& ATOM_Class = memHandle(0x6830F0).as<ATOM&>();
+auto& IconID = memHandle(0x683108).as<LPCSTR&>();
+auto& pageFlip = memHandle(0x5CA3EC).as<bool&>();
+auto& hasBorder = memHandle(0x5CA3ED).as<bool&>();
+auto& useMultiTexture = memHandle(0x5CA3EE).as<bool&>();
+auto& enableHWTnL = memHandle(0x5CA664).as<bool&>();
+auto& novblank = memHandle(0x68451D).as<bool&>();
+auto& inWindow = memHandle(0x6830D0).as<bool&>();
+auto& isMaximized = memHandle(0x6830D1).as<bool&>();
+auto& tripleBuffer = memHandle(0x6830D2).as<bool&>();
+auto& useReference = memHandle(0x6830D3).as<bool&>();
+auto& useSoftware = memHandle(0x6830D4).as<bool&>();
+auto& useAgeSoftware = memHandle(0x6830D5).as<bool&>();
+auto& useBlade = memHandle(0x6830D6).as<bool&>();
+auto& useSysMem = memHandle(0x6830D7).as<bool&>();
+auto& useInterface = memHandle(0x6830D8).as<int&>();
+auto& lpDirectDrawCreateEx = memHandle(0x684518).as<decltype(&DirectDrawCreateEx)&>();
+auto& lpDD = memHandle(0x6830A8).as<IDirectDraw7 *&>();
+auto& lpD3D = memHandle(0x6830AC).as<IDirect3D7 *&>();
+auto& lpD3DDev = memHandle(0x6830C8).as<IDirect3DDevice7 *&>();
+auto& lpdsRend = memHandle(0x6830CC).as<IDirectDrawSurface7 *&>();
+auto& gfxInterfaces = memHandle(0x683130).as<gfxInterface[8]>();
+auto& gfxInterfaceCount = memHandle(0x6844C0).as<unsigned int&>();
+auto& gfxInterfaceChoice = memHandle(0x6844C8).as<int&>();
+auto& gfxMinScreenWidth = memHandle(0x6844B0).as<int&>();
+auto& gfxMinScreenHeight = memHandle(0x6844CC).as<int&>();
+auto& gfxMaxScreenWidth = memHandle(0x6844FC).as<int&>();
+auto& gfxMaxScreenHeight = memHandle(0x6844D8).as<int&>();
+auto& gfxTexQuality = memHandle(0x6B165C).as<int&>();
+auto& gfxTexReduceSize = memHandle(0x6857D0).as<int&>();
+auto& window_fWidth = memHandle(0x6830F4).as<float&>();
+auto& window_fHeight = memHandle(0x683120).as<float&>();
+auto& window_iWidth = memHandle(0x683128).as<int&>();
+auto& window_iHeight = memHandle(0x683100).as<int&>();
+auto& window_ZDepth = memHandle(0x6830E4).as<int&>();
+auto& window_ColorDepth = memHandle(0x6830F8).as<int&>();
+auto& window_X = memHandle(0x6830EC).as<int&>();
+auto& window_Y = memHandle(0x683110).as<int&>();
+auto& ioMouse_InvWidth = memHandle(0x6A38EC).as<float&>();
+auto& ioMouse_InvHeight = memHandle(0x6A38D4).as<float&>();
+
+
+auto& StringPrinter = memHandle(0x5CECF0).as<void(*&)(LPCSTR)>();
+auto& Printer = memHandle(0x5CED24).as<void(*&)(int, LPCSTR, va_list)>();
+auto& FatalErrorHandler = memHandle(0x6A3D38).as<void(*&)(void)>();
+auto& gameClosing = memHandle(0x6B1708).as<BOOL&>();
+auto& gameState = memHandle(0x6B17C8).as<int&>();
+auto& joyDebug = memHandle(0x6A3AA8).as<int&>();
+auto& assetDebug = memHandle(0x6A3C0C).as<int&>();
+auto& gfxDebug = memHandle(0x683104).as<int&>();
+auto& audDebug = memHandle(0x6B4C24).as<int&>();
+
+
+auto gfxWindowProc = memHandle(0x4A88F0).as<WNDPROC>();
+auto DoesArgExist  = memHandle(0x4C6190).as<bool(*)(const char* name)>();
+auto GetArgInt     = memHandle(0x4C61C0).as<bool(*)(const char* name, unsigned int index, int& out)>();
+auto GetArgFloat   = memHandle(0x4C6210).as<bool(*)(const char* name, unsigned int index, float& out)>();
+auto GetArgString  = memHandle(0x4C6260).as<bool(*)(const char* name, unsigned int index, const char*& out)>();
+
+auto& obj_NoDrawThresh = memHandle(0x5C571C).as<float&>();
+auto& obj_VLowThresh = memHandle(0x5C6658).as<float&>();
+auto& obj_LowThresh = memHandle(0x5C665C).as<float&>();
+auto& obj_MedThresh = memHandle(0x5C6660).as<float&>();
+auto& sdl_VLowThresh = memHandle(0x5C5708).as<float&>();
+auto& sdl_LowThresh = memHandle(0x5C570C).as<float&>();
+auto& sdl_MedThresh = memHandle(0x5C5710).as<float&>();
+// auto& ROOT = memHandle(0x661738).as<asNode&>();
+auto& cityName = memHandle(0x6B167C).as<char[40]>();
+auto& cityName2 = memHandle(0x6B16A4).as<char[40]>();
+auto& timeOfDay = memHandle(0x62B068).as<int&>();
+auto& vehCar_bHeadlights = memHandle(0x627518).as<int&>();
+
+
+void gfxWindowMove(bool isOpen)
+{
+    HDC hDC = GetDC(NULL);
+    int screenWidth  = GetDeviceCaps(hDC, HORZRES);
+    int screenHeight = GetDeviceCaps(hDC, VERTRES);
+    ReleaseDC(0, hDC);
+
+    window_X = (screenWidth  - window_iWidth)  / 2;
+    window_Y = (screenHeight - window_iHeight) / 2;
+
+    if (isOpen)
+    {
+        MoveWindow(hWndMain, window_X, window_Y, window_iWidth, window_iHeight, 0);
+    }
+}
+
+void gfxWindowUpdate(bool isOpen)
+{
+    RECT rect;
+    GetClientRect(hWndMain, &rect);
+
+    MoveWindow(hWndMain,window_X, window_Y, (2 * window_iWidth - rect.right), (2 * window_iHeight - rect.bottom), isOpen);
+}
+
+void gfxWindowCreate(LPCSTR lpWindowName)
+{
+    if (hWndMain)
+    {
+        return;
+    }
+
+    if (lpWindowTitle)
+    {
+        lpWindowName = lpWindowTitle;
+    }
+
+    if (ATOM_Class == NULL)
+    {
+        WNDCLASSA wc =
+        {
+            CS_HREDRAW | CS_VREDRAW,    /* style */
+            gfxWindowProc,              /* lpfnWndProc */
+            0,                          /* cbClsExtra */
+            0,                          /* cbWndExtra */
+            0,                          /* hInstance */
+            LoadIconA(GetModuleHandleA(NULL), IconID ? IconID : IDI_APPLICATION),
+                                        /* hIcon */
+            LoadCursorA(0, IDC_ARROW),  /* hCursor */
+            CreateSolidBrush(NULL),     /* hbrBackground */
+            NULL,                       /* lpszMenuName */
+            "gfxWindow",                /* lpszClassName */
+        };
+
+        ATOM_Class = RegisterClassA(&wc);
+    }
+
+    DWORD dwStyle = WS_POPUP;
+
+    if (inWindow)
+    {
+        if (hWndParent)
+        {
+            dwStyle = WS_CHILD;
+        }
+        else if (hasBorder = !(DoesArgExist("noborder") || DoesArgExist("borderless")))
+        {
+            dwStyle |= (WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
+        }
+    }
+    else
+    {
+        dwStyle |= WS_SYSMENU;
+    }
+
+    // update the position
+    gfxWindowMove(false);
+
+    hWndMain = CreateWindowExA(
+        WS_EX_APPWINDOW,
+        "gfxWindow",
+        lpWindowName,
+        dwStyle,
+        window_X,
+        window_Y,
+        640,
+        480,
+        hWndParent,
+        0,
+        0,
+        0);
+
+    if (inWindow)
+    {
+        gfxWindowUpdate(false);
+    }
+
+    SetCursor(NULL);
+    ShowCursor(FALSE);
+
+    ShowWindow(hWndMain, TRUE);
+    UpdateWindow(hWndMain);
+    SetFocus(hWndMain);
+}
+
+static void SetRes(int width, int height, int cdepth, int zdepth, bool parseArgs) {
+    if (DoesArgExist("ref")) {
+        useSoftware    = 1;
+        useReference   = 1;
+    } else if (DoesArgExist("blade") || DoesArgExist("bladed")) {
+        useSoftware    = 1;
+        useBlade       = 1;
+    } else if (DoesArgExist("swage")) {
+        useSoftware    = 1;
+        useAgeSoftware = 1;
+    } else if (DoesArgExist("sw")) {
+        useSoftware    = 1;
+    }
+
+    if (DoesArgExist("sysmem")) {
+        useSysMem = 1;
+    }
+    if (DoesArgExist("triple")) {
+        tripleBuffer = 1;
+    }
+
+    if (DoesArgExist("nomultitexture") || DoesArgExist("nomt")) {
+        useMultiTexture = 0;
+    }
+    if (DoesArgExist("novblank") || DoesArgExist("novsync")) {
+        novblank = 1;
+    }
+    if (DoesArgExist("nohwtnl")) {
+        enableHWTnL = 0;
+    }
+
+    if (DoesArgExist("primary")) {
+        useInterface = 0;
+    } else {
+        GetArgInt("display", 0, useInterface);
+    }
+    if (DoesArgExist("single")) {
+        pageFlip = 0;
+    }
+
+    if (DoesArgExist("window") || DoesArgExist("windowed")) {
+        inWindow = 1;
+    } else if (DoesArgExist("fs") || DoesArgExist("fullscreen")) {
+        inWindow = 0;
+    }
+
+    int bitDepth = 0;
+    if (GetArgInt("bpp", 0, bitDepth) || GetArgInt("bitdepth", 0, bitDepth)) {
+        cdepth = bitDepth;
+        zdepth = bitDepth;
+    } else {
+        GetArgInt("cdepth", 0, cdepth);
+        GetArgInt("zdepth", 0, zdepth);
+    }
+
+    // We don't want to set the width/height if we are in a menu, it just fucks it up
+    if (gameState != 0) {
+        if (DoesArgExist("max")) {
+            HDC hDC = GetDC(NULL);
+            width  = GetDeviceCaps(hDC, HORZRES);
+            height = GetDeviceCaps(hDC, VERTRES);
+            ReleaseDC(0, hDC);
+        } else {
+            GetArgInt("width",  0, width);
+            GetArgInt("height", 0, height);
+        }
+
+        // DoesArgExist("width",  0, &width);
+        // DoesArgExist("height", 0, &height);
+    }
+
+    useSysMem = useSoftware;
+
+    window_iWidth  = width;
+    window_iHeight = height;
+
+    window_fWidth  = float(width);
+    window_fHeight = float(height);
+
+    window_ColorDepth = cdepth;
+    window_ZDepth     = zdepth;
+
+    sm_Allow32 = (cdepth == 32);
+
+    if (lpDD)
+    {
+        if (inWindow)
+        {
+            gfxWindowMove(true);
+            gfxWindowUpdate(true);
+        } else {
+            DDSURFACEDESC2 ddSurfaceDesc;
+
+            ddSurfaceDesc.dwSize = 0x7C;
+
+            if ((lpDD->GetDisplayMode(&ddSurfaceDesc) != DD_OK) || (ddSurfaceDesc.dwWidth != window_iWidth) || (ddSurfaceDesc.dwHeight != window_iHeight))
+            {
+                if (lpDD->SetDisplayMode(
+                    window_iWidth,
+                    window_iHeight,
+                    window_ColorDepth,
+                    0,
+                    0) != DD_OK) {
+                    DebugPrint("[gfxPipeline::SetRes]: SHIT! Failed to set the display mode!");
+                }
+            }
+        }
+    }
+
+    ioMouse_InvWidth  = (1.0f / window_fWidth);
+    ioMouse_InvHeight = (1.0f / window_fHeight);
+}
+
+auto $DeviceCallback = memHandle(0x4AC3D0).as<LPD3DENUMDEVICESCALLBACK7>();
+auto $ResCallback    = memHandle(0x4AC6F0).as<LPDDENUMMODESCALLBACK2>();
+
+BOOL __stdcall AutoDetectCallback(GUID *lpGUID, LPSTR lpDriverDescription, LPSTR lpDriverName, LPVOID lpContext)
+{
+    if (lpDirectDrawCreateEx(lpGUID, (LPVOID*)&lpDD, IID_IDirectDraw7, nullptr) == DD_OK)
+    {
+        gfxInterface *gfxInterface = &gfxInterfaces[gfxInterfaceCount++];
+
+        std::strncpy(gfxInterface->Name, lpDriverDescription, std::size(gfxInterface->Name));
+
+        gfxInterface->DeviceCaps = 1;
+        gfxInterface->AcceptableDepths = gfxInterface::gfxDepthFlags::Depth32;
+
+        DDDEVICEIDENTIFIER2 ddDeviceIdentifier = { NULL };
+
+        if (lpDD->GetDeviceIdentifier(&ddDeviceIdentifier, 0) == DD_OK)
+        {
+            gfxInterface->VendorID  = ddDeviceIdentifier.dwVendorId;
+            gfxInterface->DeviceID  = ddDeviceIdentifier.dwDeviceId;
+            gfxInterface->GUID      = ddDeviceIdentifier.guidDeviceIdentifier;
+        }
+
+        if (lpDD->QueryInterface(IID_IDirect3D7, (LPVOID*)&lpD3D) == DD_OK)
+        {
+            lpD3D->EnumDevices($DeviceCallback, gfxInterface);
+            lpD3D->Release();
+
+            lpD3D = nullptr;
+        }
+
+        gfxInterface->DeviceType        = gfxInterface::gfxDeviceType::HardwareWithTnL;
+
+        gfxInterface->ResolutionCount   = 0;
+        gfxInterface->ResolutionChoice  = 0;
+
+        DWORD availableMemory = 0x40000000; // 1GB = 1024 * 1024 * 1024
+
+        DDSCAPS2 ddsCaps = { NULL };
+
+        ddsCaps.dwCaps = DDSCAPS_VIDEOMEMORY | DDSCAPS_LOCALVIDMEM;
+
+        if (lpDD->GetAvailableVidMem(&ddsCaps, &availableMemory, NULL) != DD_OK)
+            DebugPrint("  Couldn't get video memory, using default");
+
+        DebugPrint("  Total video memory: %dMB", (availableMemory >> 20));
+
+        gfxInterface->AvailableMemory = availableMemory;
+
+        gfxMaxScreenWidth = 0;
+        gfxMaxScreenHeight = 0;
+
+        lpDD->EnumDisplayModes(0, 0, gfxInterface, $ResCallback);
+        lpDD->Release();
+
+        lpDD = nullptr;
+    }
+
+    return TRUE;
+}
+
+class memSafeHeap;
+
+auto $memSafeHeap_Init = memHandle(0x577210).as<void(memSafeHeap::*)(void*, unsigned int, bool, bool, bool)>();
+
+class memSafeHeap
+{
+public:
+    void Init(void *memAllocator, unsigned int heapSize, bool p3, bool p4, bool checkAlloc)
+    {    
+        int heapSizeMB = 128;
+        GetArgInt("heapsize", 0, heapSizeMB);
+
+        heapSize = heapSizeMB * (1024 * 1024);
+
+        DebugPrint("[memSafeHeap::Init]: Allocating %dMB heap (%d bytes)\n", heapSizeMB, heapSize);
+        return (this->*$memSafeHeap_Init)(memAllocator, heapSize, p3, p4, checkAlloc);
+    }
+};
+
+auto& __VtResumeSampling = memHandle(0x5E0CC4).as<void(*&)(void)>();
+auto& __VtPauseSampling  = memHandle(0x5E0CD8).as<void(*&)(void)>();
+
+void Initialize()
+{
+    DebugPrint("Initialization Begin");
+ 
+    std::clock_t begin = std::clock();
+
+    CreateHook("gfxPipeline::SetRes",          "Custom implementation allowing for more control of the window",                0x4A8CE0, &SetRes,             HookType::JMP);
+    CreateHook("AutoDetectCallback",           "Replaces the default AutoDetect method with a much faster one",                0x4AC030, &AutoDetectCallback, HookType::JMP);
+    CreateHook("memSafeHeap::Init",            "Adds '-heapsize' parameter that takes a size in megabytes. Defaults to 128MB", 0x4015DD, &memSafeHeap::Init,  HookType::CALL);
+    CreateHook("gfxPipeline::gfxWindowCreate", "Custom implementation allowing for more control of the windo.",                0x4A8A90, &gfxWindowCreate,    HookType::JMP);
+
+    memHandle(0x4F136E).write_args_vp<std::uint8_t, std::uint8_t>(0x90, 0x90); // Enable custom in windowed mode
+
+    DebugPrint("Initialize Completed in %.2f Seconds", double(std::clock() - begin) / CLOCKS_PER_SEC);
+}
---
+++ Midtown Revisited/memHandle.h
@@ -0,0 +1,398 @@
+#pragma once
+
+template <typename In, typename Out>
+inline Out pointer_cast(In in)
+{
+    static_assert(std::is_pointer<In>::value || std::is_member_function_pointer<In>::value, "Invalid Type");
+
+    union union_caster
+    {
+        In in;
+        Out out;
+
+        union_caster(In in)
+            : in(in)
+        { }
+    };
+
+    return union_caster(in).out;
+}
+
+class memHandle
+{
+protected:
+    void* _handle;
+
+public:
+    memHandle()
+        : _handle(nullptr)
+    { }
+
+    memHandle(std::nullptr_t null)
+        : _handle(null)
+    { }
+
+    template <typename T, typename = std::enable_if<std::is_pointer<T>::value || std::is_member_function_pointer<T>::value>::type>
+    memHandle(T p)
+        : _handle(pointer_cast<T, void*>(p))
+    { }
+
+    memHandle(const std::uintptr_t p)
+        : _handle(reinterpret_cast<void*>(p))
+    { }
+
+    memHandle(const memHandle& copy)
+        : _handle(copy._handle)
+    { }
+
+    template <typename T>
+    std::enable_if_t<std::is_pointer<T>::value || std::is_member_function_pointer<T>::value, T> as() const
+    {
+        return pointer_cast<void*, T>(this->_handle);
+    }
+
+    template <typename T>
+    std::enable_if_t<std::is_lvalue_reference<T>::value, T> as() const
+    {
+        return *this->as<std::remove_reference_t<T>*>();
+    }
+
+    template <typename T>
+    std::enable_if_t<std::is_array<T>::value, T&> as() const
+    {
+        return this->as<T&>();
+    }
+
+    template <typename T>
+    std::enable_if_t<std::is_same<T, std::uintptr_t>::value, T> as() const
+    {
+        return reinterpret_cast<std::uintptr_t>(this->as<void*>());
+    }
+
+    template <typename T>
+    std::enable_if_t<std::is_same<T, std::intptr_t>::value, T> as() const
+    {
+        return reinterpret_cast<std::intptr_t>(this->as<void*>());
+    }
+
+    bool operator==(const memHandle& rhs) const
+    {
+        return this->as<void*>() == rhs.as<void*>();
+    }
+
+    bool operator!=(const memHandle& rhs) const
+    {
+        return this->as<void*>() != rhs.as<void*>();
+    }
+
+    bool operator>(const memHandle& rhs) const
+    {
+        return this->as<void*>() > rhs.as<void*>();
+    }
+
+    bool operator<(const memHandle& rhs) const
+    {
+        return this->as<void*>() < rhs.as<void*>();
+    }
+
+    bool operator>=(const memHandle& rhs) const
+    {
+        return this->as<void*>() >= rhs.as<void*>();
+    }
+
+    bool operator<=(const memHandle& rhs) const
+    {
+        return this->as<void*>() <= rhs.as<void*>();
+    }
+
+    operator void*() const
+    {
+        return this->as<void*>();
+    }
+
+    memHandle save(memHandle& out) const
+    {
+        return (out = *this);
+    }
+
+    template <typename T>
+    std::enable_if_t<std::is_integral<T>::value, memHandle> offset(const T offset) const
+    {
+        return this->as<std::uintptr_t>() + offset;
+    }
+
+    template <typename T>
+    memHandle rip(const T ipoffset) const
+    {
+        return this->offset(ipoffset).offset(this->as<int&>());
+    }
+
+    memHandle translate(const memHandle from, const memHandle to) const
+    {
+        return to.offset(this->as<std::intptr_t>() - from.as<std::intptr_t>());
+    }
+
+    bool protect(const std::size_t size, const std::uint32_t newProtect, const std::uint32_t* oldProtect)
+    {
+        return VirtualProtect(this->as<void*>(), size, (DWORD) newProtect, (DWORD*) &oldProtect) == TRUE;
+    }
+
+    bool nop(const std::size_t size)
+    {
+        std::uint32_t oldProtect;
+
+        if (this->protect(size, PAGE_EXECUTE_READWRITE, &oldProtect))
+        {
+            std::memset(this->as<void*>(), 0x90, size);
+
+            this->protect(size, oldProtect, nullptr);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    template <typename T>
+    void write(const T value)
+    {
+        static_assert(std::is_trivially_copyable<T>::value, "Type is not trivially copyable");
+
+        this->as<T&>() = value;
+    }
+
+    template <typename... T>
+    void write_args(const T... args)
+    {
+        std::uintptr_t off = 0;
+
+        (void) std::initializer_list<int>
+        {
+            0, (this->offset(off).write(args), off += sizeof(args), 0)...
+        };
+    }
+
+    template <typename T>
+    bool write_vp(const T value)
+    {
+        std::uint32_t oldProtect;
+
+        auto size = sizeof(value);
+
+        if (this->protect(size, PAGE_EXECUTE_READWRITE, &oldProtect))
+        {
+            this->write(value);
+
+            this->protect(size, oldProtect, nullptr);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    template <typename... T>
+    bool write_args_vp(const T... args)
+    {
+        std::uint32_t oldProtect;
+
+        auto size = std::valarray<std::size_t>({ sizeof(args)... }).sum();
+
+        if (this->protect(size, PAGE_EXECUTE_READWRITE, &oldProtect))
+        {
+            this->write_args(args...);
+
+            this->protect(size, oldProtect, nullptr);
+
+            return true;
+        }
+
+        return false;
+    }
+};
+
+class memPattern
+{
+protected:
+    class memNibble
+    {
+    public:
+        std::uint8_t value;
+        std::uint8_t offset;
+    };
+
+    std::uint8_t _count;
+    memNibble    _nibbles[64];
+
+    static const constexpr std::uint8_t hex_char_table[256] =
+    {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+public:
+    constexpr memPattern(const char* string)
+        : _nibbles { }
+        , _count(0)
+    {
+        while (*string)
+        {
+            for (; (*string) && (*string == ' '); ++string);
+
+            if (*string != '?')
+            {
+                this->_nibbles[this->_count].value      = hex_char_table[string[0]] << 4 | hex_char_table[string[1]];
+                this->_nibbles[this->_count + 1].offset = this->_nibbles[this->_count].offset;
+
+                this->_count++;
+            }
+
+            this->_nibbles[this->_count].offset++;
+
+            for (; (*string) && (*string != ' '); ++string);
+        }
+    }
+
+    constexpr std::size_t size() const
+    {
+        return this->_count;
+    }
+
+    constexpr std::size_t length() const
+    {
+        return this->_nibbles[this->size()].offset;
+    }
+
+    memHandle scan(const memHandle base, const memHandle end, std::size_t index = 0) const
+    {
+        for (memHandle i = base; i < end.offset(-static_cast<std::intptr_t>(this->length())); i = i.offset(1))
+        {
+            bool found = true;
+
+            for (std::size_t j = 0; j < this->size(); ++j)
+            {
+                if (this->_nibbles[j].value != i.offset(this->_nibbles[j].offset).as<std::uint8_t&>())
+                {
+                    found = false;
+
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                if (index == 0)
+                {
+                    return i;
+                }
+                else
+                {
+                    --index;
+                }
+            }
+        }
+
+        return nullptr;
+    }
+
+    std::vector<memHandle> scan_all(const memHandle base, const memHandle end) const
+    {
+        std::vector<memHandle> results;
+
+        for (memHandle i = base; i = this->scan(i, end); i = i.offset(1))
+        {
+            results.push_back(i);
+        }
+
+        return results;
+    }
+};
+
+class memModule
+{
+protected:
+    memHandle   _base;
+    std::size_t _size;
+
+public:
+    memModule(memHandle base)
+        : _base(base)
+        , _size(base.offset(base.as<IMAGE_DOS_HEADER&>().e_lfanew).as<IMAGE_NT_HEADERS&>().OptionalHeader.SizeOfImage)
+    { }
+
+    static memModule named(const char* name)
+    {
+        return GetModuleHandleA(name);
+    }
+
+    static memModule named(const wchar_t* name)
+    {
+        return GetModuleHandleW(name);
+    }
+
+    static memModule named(const std::nullptr_t null)
+    {
+        return GetModuleHandleA(null);
+    }
+
+    static memModule main()
+    {
+        return memModule::named(nullptr);
+    }
+
+    memHandle base() const
+    {
+        return this->_base;
+    }
+
+    std::size_t size() const
+    {
+        return this->_size;
+    }
+
+    template <typename T>
+    memHandle offset(const T offset) const
+    {
+        return this->base().offset(offset);
+    }
+
+    memHandle distance(const memHandle pointer) const
+    {
+        return pointer.as<std::uintptr_t>() - this->base().as<std::uintptr_t>();
+    }
+
+    memHandle end() const
+    {
+        return this->offset(this->size());
+    }
+
+    memHandle scan(const memPattern& pattern, std::size_t index = 0) const
+    {
+        return pattern.scan(this->base(), this->end(), index);
+    }
+
+    std::vector<memHandle> scan_all(const memPattern& pattern) const
+    {
+        return pattern.scan_all(this->base(), this->end());
+    }
+
+    bool contains(const memHandle address) const
+    {
+        return (address >= this->base()) && (address < this->end());
+    }
+};
+
